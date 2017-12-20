0dfbd777a8ad0c6d4e856d2aeb3e2fad45c8ba61
saurabh13209 <agrawal99saurabh@gmail.com>
2017-12-20 18:47:10 +0530

    New Item Activity bug fixed

--- app/src/main/java/com/twelo/mylist/About.java
+++ app/src/main/java/com/twelo/mylist/About.java
@@ -2,12 +2,18 @@ package com.twelo.mylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class About extends AppCompatActivity {
-
+    android.support.v7.widget.Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
+        TextView title = (TextView)findViewById(R.id.Main_Menu_Title);
+        title.setText("About");
+        setSupportActionBar(toolbar);
     }
 }
